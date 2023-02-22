37faf45f554ccddb59710986caab13567c6f9f36
George Schafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-02-22 15:06:47 +0000

    modified ReplitDBFunctions formatting

---
+++ gitPushCommitRepos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Navigate to the ReplitDBFunctions folder
+cd ReplitDBFunctions
+
+# Commit changes to git
+git commit -m
+
+# Push changes to Git
+git push origin 20230222-jsBookie-backend
+
+# Navigate to root
