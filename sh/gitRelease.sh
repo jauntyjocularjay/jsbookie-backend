bed8960ef5b5f725ed617a9361511e47c802f9ad
GeorgeSchafer <>
2023-10-09 18:36:50 +0000

    Release Merge

---
+++ sh/gitRelease.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git commit . -m "Release Merge"
+git push
+
+git checkout Dev
+git merge ReplitBranch -m "Release Merge"
+git push
+
+git checkout main
+git merge Dev -m "Release Merge"
+git push
+
+git checkout Release
+git merge main -m "Release Merge"
+git push
+
+git checkout ReplitBranch
